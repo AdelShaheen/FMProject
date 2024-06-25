ec9e68464b653e6d8058f198ed1f3e3ae389546a
Adel Mohamed Abdelhay Shaheen <145797771+AdelShaheen@users.noreply.github.com>
2024-06-25 15:44:35 +0300

    Update FM Radio.m
    Added frequency domain graphs and modified comments.

--- FM Radio.m
+++ FM Radio.m
@@ -1,73 +1,137 @@
-% Define the parameters
-fc1 = 1000; % Carrier frequency of the first signal
-fc2 = 2000; % Carrier frequency of the second signal
-fs = 8000; % Sampling frequency
-t = 0:1/fs:1-1/fs; % Time vector
-fDev = 50;
+% Define parameters
+fc1 = 1000;         % Carrier frequency of the first signal
+fc2 = 2000;         % Carrier frequency of the second signal
+fDev = 50;          % Frequency deviation
+fs = 8000;          % Sampling rate
+gain = 2;           % Amplifier gain
+t = 0:1/fs:1-1/fs;  % Time vector
 
-% Define the signals
-x1 = sin(2*pi*100*t); % First sine wave signal
-t=linspace(0,length(x1 )/fs,length(x1));
+% Define the input signals
+x1 = sin(2*pi*100*t);   % First signal
+x2 = sin(2*pi*200*t);   % Second signal
+
+% Plot the input signals in time domain
+t = linspace(0, length(x1)/fs, length(x1));
 figure;
+subplot(1,2,1)
 plot(t,x1);
-title('x1')
+xlim([0 0.1])
+title('ٍSignal 1 (Time Domain)')
 xlabel('time')
 ylabel('amplitude')
 pause(2);
 sound(x1, fs);
 pause(5);
 
-
-x2 = sin(2*pi*200*t); % Second sine wave signal
-t=linspace(0,length(x2)/fs,length(x2));
-figure;
+t = linspace(0, length(x2)/fs, length(x2));
+subplot(1,2,2)
 plot(t,x2);
-title('x2')
+xlim([0 0.1])
+title('Signal 2 (Time Domain)')
 xlabel('time')
 ylabel('amplitude')
 pause(2);
 sound(x2, fs);
 pause(5);
 
-% Modulate the signals using VCO circuits
-y1 = fmmod(x1, fc1, fs,fDev); % Modulated signal 1
-y2 = fmmod(x2,fc2, fs,fDev); % Modulated signal 2
+% Find the frequency domain representation of the signal
+X1 = fftshift(fft(x1));     
+X2 = fftshift(fft(x2));
 
-% Sum the two modulated signals
-y = y1 + y2;
+% Plot the input signals in frequency domain
+f = fs/2*linspace(-1, 1, fs);
+figure;
+subplot(1,2,1)
+plot(f, abs(X1));
+xlim([-fs/2 fs/2])
+title('Signal 1 (Frequency Domain)')
+xlabel('frequency')
+ylabel('amplitude')
 
-% Pass the modulated signals through a power amplifier
-gain = 2; % Amplifier gain
+subplot(1,2,2)
+plot(f, abs(X2));
+xlim([-fs/2 fs/2])
+title('Signal 2 (Frequency Domain)')
+xlabel('frequency')
+ylabel('amplitude')
+pause(3);
+
+% Modulate the input singals
+y1 = fmmod(x1, fc1, fs, fDev);      % Modulation of signal 1
+y2 = fmmod(x2, fc2, fs, fDev);      % Modulation of signal 2
+y = y1 + y2;                        % Summation of the two modulated signals for transmission
+
+% Calculate the Fast Fourier Transform of modulated signals
+Y1 = fftshift(fft(y1));     
+Y2 = fftshift(fft(y2));
+Y = fftshift(fft(y));
+
+% Plot the modulated signals
+figure;
+subplot(2,2,1)
+plot(f, abs(Y1));
+xlim([-fs/2 fs/2])
+title('Modulation of Signal 1')
+xlabel('frequency')
+ylabel('amplitude')
+
+subplot(2,2,2)
+plot(f, abs(Y2));
+xlim([-fs/2 fs/2])
+title('Modulation of Signal 2')
+xlabel('frequency')
+ylabel('amplitude')
+
+t = linspace(0,length(y)/fs,length(y));
+subplot(2,2,3)
+plot(t, y);
+xlim([0 0.1])
+title('Transmission Signal (Time)')
+xlabel('Time')
+ylabel('amplitude')
+
+subplot(2,2,4)
+plot(f, abs(Y));
+xlim([-fs/2 fs/2])
+title('Transmission Signal (Freq)')
+xlabel('frequency')
+ylabel('amplitude')
+pause(3);
+
+% Amplify the transmission signal
 y_amp = y * gain;
 
-% Transmit the amplified signal
-
-% Demodulate the transmitted signal using a phase-locked loop (PLL)
-f0 = fc1 + fc2; % Carrier frequency of the transmitted signal
-fDev = 50; % Frequency deviation of the transmitted signal
-y_demod = fmmod(y_amp, f0, fs, fDev); % Demodulated signal
-
-% Pass the demodulated signal through a selection circuit that chooses between the two input signals using a switch
-switch_pos = 0; % Switch position (0 or 1)
+% Select the desired signal
+switch_pos = 1;         % Switch position (0 or 1)
 if switch_pos == 0
     y_sel = y2;
-    f0=fc2;
+    f0 = fc2;
 else
     y_sel = y1;
-    f0=fc1;
+    f0 = fc1;
 end
 
-% Demodulate the selected signal using another phase-locked loop (PLL)
-%f0 = fc1; % Carrier frequency of the selected signal
-fDev = 50; % Frequency deviation of the selected signal
-y_demod_sel = fmdemod(y_sel, f0, fs, fDev); % Demodulated signal
+% Demodulate the selected signal
+y_demod = fmdemod(y_sel, f0, fs, fDev); % Demodulated signal
 
-% Pass the demodulated selected signal through another power amplifier and output in speaker
-y_audio_sel = y_demod_sel * gain; % Amplified audio signal
-sound(y_audio_sel, fs); % Output audio signal in speaker
-t=linspace(0,length(y_audio_sel )/fs,length(y_audio_sel));
+% Amplify and play the output signal
+y_audio_sel = y_demod * gain;           % Amplified output audio signal
+sound(y_audio_sel, fs);                 % Play signal through speaker
+
+% Plot the output signal
+t = linspace(0, length(y_audio_sel)/fs, length(y_audio_sel));
 figure;
-plot(t,y_audio_sel );
-title('y_audio_sel ')
+subplot(1,2,1)
+plot(t, y_audio_sel);
+xlim([0 0.1])
+title('Output Signal (Time)')
 xlabel('time')
+ylabel('amplitude')
+
+Y_demod = fftshift(fft(y_demod));
+subplot(1,2,2)
+plot(f, abs(Y_demod));
+xlim([-fs/2 fs/2])
+title('Output Signal (Freq)')
+xlabel('frequency')
 ylabel('amplitude')
